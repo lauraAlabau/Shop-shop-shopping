723de1302853a005b9136fbc8eb41c29afd5d1ad
lauraAlabau <arlaura3@gmail.com>
2022-08-09 18:03:40 +0200

    Shop web in Next with TypeScript and MaterialUI

---
+++ components/layout/ShopLayout.tsx
@@ -0,0 +1,50 @@
+import Head from "next/head";
+
+import { FC } from "react";
+import { Navbar } from "../ui";
+
+import styles from "./ShopLayout.module.css";
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+  children?: React.ReactNode;
+}
+
+export const ShopLayout: FC<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+
+        {/* SEO para redes sociales */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={pageDescription} />
+
+        {imageFullUrl && ( 
+          // Si existe la imagen se agrega
+          <meta property="og:image" content={imageFullUrl} />
+        )}
+
+      </Head>
+      
+      <nav>
+        <Navbar />
+      </nav>
+
+      {/* TODO: Sidebar */}
+
+      <main className={styles.shop_layout_main}>{children}</main>
+
+      {/* Footer */}
+      <footer>{/* TODO: custom footer */}</footer>
+    </>
+  );
+};
