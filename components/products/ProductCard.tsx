3950ea8b71b51fbac293a3c6790d39fb11987788
lauraAlabau <arlaura3@gmail.com>
2022-08-09 20:25:29 +0200

    Added product info page aka slug

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,12 +1,15 @@
+import { FC, useMemo, useState } from "react";
+import NextLink from "next/link";
+
 import {
   Box,
   Card,
   CardActionArea,
   CardMedia,
   Grid,
+  Link,
   Typography,
 } from "@mui/material";
-import { FC, useMemo, useState } from "react";
 import { IProduct } from "../../interfaces";
 
 interface Props {
@@ -14,7 +17,6 @@ interface Props {
 }
 
 export const ProductCard: FC<Props> = ({ product }) => {
-
   const [isHovered, setIsHovered] = useState(false);
 
   const productImage = useMemo(() => {
@@ -32,19 +34,23 @@ export const ProductCard: FC<Props> = ({ product }) => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component='img'
-            className='fadeIn'
-            image={ productImage }
-            alt={product.title}
-          />
-        </CardActionArea>
+        <NextLink href='/product/slug' passHref prefetch={ false }>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className="fadeIn"
+                image={productImage}
+                alt={product.title}
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box sx={{ mt: 1 }} className="fadeIn">
         <Typography fontWeight={700}>{product.title}</Typography>
-        <Typography fontWeight={500}>€{product.price}</Typography>
+        <Typography fontWeight={500}>{product.price} €</Typography>
       </Box>
     </Grid>
   );
